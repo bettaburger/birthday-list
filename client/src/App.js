52dbb7e3137ea21b7976e2800f631265e60e1ea0
stephanie <stephanieha2021@gmail.com>
2024-02-27 22:41:11 -0500

    persist state to localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Time from './components/Time.js';
 import List from './components/List.js';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Birthday Tracker</h1>
       <Time/>
+      <h4>Track your friends or family birthdays, start by adding a person below!</h4>
       <div className="item">
         <List/>
       </div>
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "../styles/listItem.css"
 import axios from 'axios'
 
@@ -8,9 +8,17 @@ function List() {
     const [firstName, setFirstName] = useState('');      // First names.
     const [lastName, setLastName] = useState('');        // Last names.
     const [date, setDate] = useState('');                // Dates.
-    const [birthday, setBirthday] = useState([]);        // Birthdays.
+    const [birthday, setBirthday] = useState([]);        // Birthday items.
     const [warning, setWarning] = useState('');          // No input warning.
     const [add, setAdd] = useState('');                  // Birthday has been added. 
+
+    useEffect(() => {
+        setBirthday(JSON.parse(window.localStorage.getItem('birthday')));
+    }, [])
+
+    useEffect(() => {
+        window.localStorage.setItem('birthday', JSON.stringify(birthday))
+    },[birthday])
 
     const axiosPostData = async() => {
         const postData = {
@@ -22,87 +30,83 @@ function List() {
         .then(response => setAdd(<p style={{ color: '#A6C28B' }}>{response.data}</p>))   
     }
 
-   // Handle submit and prevent page default submit button refresh.
-   const handleSubmit = (e) => {
-        e.preventDefault();
-        if (firstName === '' || lastName === '' || date === '') {
-            setWarning('Make sure to fill all fields.');
-            return;
-        } else {
-            setBirthday([...birthday, { firstName, lastName, date }]);
-            setFirstName('');
-            setLastName('');
-            setDate('');
-            setWarning('');
+    // Handle submit and prevent page default submit button refresh.
+    const handleSubmit = (e) => {
+            e.preventDefault();
+            if (firstName === '' || lastName === '' || date === '') {
+                setWarning('Make sure to fill all fields.');
+                return;
+            } else {
+                setBirthday([...birthday, { firstName, lastName, date }]);
+                setFirstName('');
+                setLastName('');
+                setDate('');
+                setWarning('');
+                setAdd('');
+                //console.info(birthday);
+            }
             setAdd('');
+            axiosPostData();
         }
-        setAdd('');
-        axiosPostData();
+
+    // Creating a new array excluding the person to be deleted
+    // Updates the list of birthdays. 
+    const handleDelete = (person) => {
+        const updatedBirthday = birthday.filter((_, currentPerson) => currentPerson !== person); 
+        setBirthday(updatedBirthday);
     }
 
-   // Creating a new array excluding the person to be deleted
-   // Updates the list of birthdays. 
-   const handleDelete = (person) => {
-    const updatedBirthday = birthday.filter((_, currentPerson) => currentPerson !== person); 
-    setBirthday(updatedBirthday);
-   }
-
-   // Change handlers
-   const handleFirstName = (e) => {
-    setFirstName(e.target.value);
-   }
-   const handleLastName = (e) => {
-    setLastName(e.target.value);
-   }
-   const handleDate = (e) => {
-    setDate(e.target.value);
-   }
-
- return (
-   <div>
-       <form onSubmit={handleSubmit}>
-           <div>
-               <label> Track your friends or family birthdays, start by adding a person below!
-                <p></p>
-                   <input
-                       type="text"
-                       placeholder="First Name"
-                       value={firstName}
-                       onChange={handleFirstName} 
+    // Change handlers
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
+    const handleDate = (e) => {
+        setDate(e.target.value);
+    }
+    
+    return (   
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={handleFirstName} 
                     />
                     <input
-                       type="text"
-                       placeholder="Last Name"
-                       value={lastName}
-                       onChange={handleLastName} 
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={handleLastName} 
                     />
                     <input
-                       type="date"
-                       value={date}
-                       onChange={handleDate} 
+                    type="date"
+                    value={date}
+                    onChange={handleDate} 
                     />
                     <button type="add">Add</button>
-               </label>
-           </div>
-       </form>
+            </div>
+        </form>
 
-        <p style={{ color: '#a42525' }}>{warning}</p>
-        <p>{add}</p>
+            <h4 style={{ color: '#a42525' }}>{warning}</h4>
+            <h4>{add}</h4>
+            {console.log(birthday)}
 
-        {birthday.map((birthday, index) => (
-            <div key = {index}>
-            <h3> {birthday.firstName} {birthday.lastName}'s birthday is on 
-            <span style ={{color: '#CBA52F'}}>
-            &nbsp;{birthday.date}
-            <button className={styles.deleteButton}
-             type="delete" onClick={() => handleDelete(index)}></button>
-            </span>
-            </h3>
-        </div>
-        ))}
-   </div>
- )
-}
-
-
-export default List
+            {birthday.map((birthday, index) => (
+                <div key = {index}>
+                <h3> {birthday.firstName} {birthday.lastName}'s birthday is on 
+                <span style ={{color: '#CBA52F'}}>
+                &nbsp;{birthday.date}
+                <button className={styles.deleteButton}
+                type="delete" onClick={() => handleDelete(index)}></button>
+                </span>
+                </h3>
+            </div>
+            ))}
+    </div>
+    )}
+    export default List
